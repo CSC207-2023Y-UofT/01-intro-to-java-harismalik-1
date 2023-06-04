9a3779a80b058d6f9b5f2534b4e92e739c764148
Haris Malik <112772076+harismalik-1@users.noreply.github.com>
2023-06-03 20:40:36 -0400

    Exercise completed!

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -141,6 +146,12 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String words[] = to_split.split(" ");
+
+        for (int i = 0; i < 7; i++){
+            ret.append(words[i].charAt(0));
+        }
+
 
         // Fill in the rest of the body here
 
@@ -171,6 +182,14 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        if (arr.length < 1 || arr.length == 1){
+            return 0;
+        }
+        for (int i = 0; i < arr.length; i++){
+            if (i % 2 != 0){
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
